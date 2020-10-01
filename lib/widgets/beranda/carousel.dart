3f741a0111d6f4a2ea2d2c624bb9e84c644b4b32
wilyanto <wilyanto24@gmail.com>
2020-10-02 01:20:25 +0700

    hotfix: make Beranda Carousel can be scrolled vertically

--- lib/widgets/beranda/carousel.dart
+++ lib/widgets/beranda/carousel.dart
@@ -35,6 +35,7 @@ class _BerandaCarouselState extends State<BerandaCarousel> {
 
     return ListView(
       shrinkWrap: true,
+      physics: NeverScrollableScrollPhysics(),
       children: <Widget>[
         _carouselImages
       ],
